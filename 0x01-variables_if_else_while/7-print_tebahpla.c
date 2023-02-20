8983e9b25cdf28aac158014c0a46d0dbcc218747
Cathrine <Motlalepula232@gmail.com>
2023-02-20 09:02:18 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l <= 'a'; l--)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
