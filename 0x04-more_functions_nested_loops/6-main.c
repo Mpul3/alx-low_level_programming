c12bc68d4778512f2c2b66f42215bb2039dc4c10
Cathrine <Motlalepula232@gmail.com>
2023-02-24 15:44:57 -0800

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/6-main.c
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -3,10 +3,14 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
-	print_numbers();
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
 	return (0);
 }
+
