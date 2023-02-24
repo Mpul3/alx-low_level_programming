0ece09133c52669c2cf4258a6c87ce686c6327bb
Cathrine <Motlalepula232@gmail.com>
2023-02-24 11:54:37 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -4,15 +4,15 @@
 /**
  * main - check the code.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
 	char c;
 
 	c = 'A';
-	printf("%d\n", c, _isupper(c));
+	printf("%c: %d\n", c, _isupper(c));
 	c = 'a';
-	printf("%d\n", c, _isupper(c));
+	printf("%c: %d\n", c, _isupper(c));
 	return (0);
 }
